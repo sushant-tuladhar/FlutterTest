5bcb21d8641f6949c6c0989445b8617147dcc172
Sushant Tuladhar <tuladharsushant01@gmail.com>
2023-06-14 19:51:15 +0545

    Added the test code with confirmation dialog

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -6,27 +6,28 @@ import 'package:initial_fluttere2e/main.dart' as app;
 void main(){
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+
   group('end to end Testing', () {
     testWidgets('Tap on the floating icon button and verify the logic here',(tester) async {
       app.main();
       await tester.pumpAndSettle();
 
       // VERIFY the counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-
-      // Finds the floating action button to tap on
-      final Finder fab=find.byTooltip('Increment');
+      expect(find.text('Next gen data here'), findsOneWidget);
+      expect('Sushant', 'Sushant');
       
-      // Emulate a tap on the floating action button
-      await tester.tap(fab);
 
-      // Trigger a frame
-      await tester.pumpAndSettle();
+      // // Finds the floating action button to tap on
+      // final Finder fab=find.byTooltip('Increment');
+      
+      // // Emulate a tap on the floating action button
+      // await tester.tap(fab);
 
-      // Verify the counter increments by 1
-      expect(find.text('2'), findsOneWidget);
+      // // Trigger a frame
+      // await tester.pumpAndSettle();
 
-      // Verify the s
+      // // Verify the counter increments by 1
+      // expect(find.text('2'), findsOneWidget);
     });
    });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
   ));
 }
 
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,20 +19,31 @@ class Home extends StatelessWidget {
       ),
       backgroundColor: Colors.deepOrange,
     ),
-    body:const Center(
-      child: Text('Hello world !!!',
-      style: TextStyle(
-        fontSize: 22.0,
-        fontWeight: FontWeight.bold,
-        letterSpacing: 3.0,
-        decorationStyle: TextDecorationStyle.dashed,
-        fontFamily: 'IndieFlower'
-       ),),
+    body:  Center(
+     child: GridView.count(crossAxisCount: 1,
+      children: const <Widget>[
+        Card(color:Colors.amberAccent, child: Image(image: NetworkImage('https://images.unsplash.com/photo-1532693322450-2cb5c511067d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2788&q=80'))),
+        Card(color: Colors.cyan, child: Image(image: AssetImage('assets/photo.avif'))),
+        Card(color: Colors.indigo, child: Image(image: AssetImage('assets/pic2.avif')),),
+        Card(color: Colors.deepOrange,child: Image(image: AssetImage('assets/pic3.avif')),),
+        Card(color: Colors.black12,child: Image(image: AssetImage('assets/pic4.avif')),),
+        Card(color:Colors.amber,child: Icon(Icons.airport_shuttle),)
+      ],
+     ),
     ),floatingActionButton: FloatingActionButton(
-      onPressed: () => {debugPrint('Hello check and works fine')},
-      tooltip: 'Test',
-      backgroundColor: Colors.deepOrange,
+      onPressed: () async {
+        await showDialog(context: context, builder: (context)=> AlertDialog(
+          title: const Text('Confirmation'),
+          content: const Text('Please click on the go back button'),
+          actions: [
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            }, child: const Text('Exit'))
+          ],
+        ));
+      } ,
       child: const Icon(Icons.add),
+      tooltip: 'Add_Button',
     ),);
   }
 }
